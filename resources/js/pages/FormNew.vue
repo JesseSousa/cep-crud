df26ec9ccb1aadbd09b1e3ff6d712d6f3a3f3c3f
JesseSousa <rhadamesjesse14rj@gmail.com>
2022-04-14 15:21:56 -0300

    add functionality to FormNew page

--- resources/js/pages/FormNew.vue
+++ resources/js/pages/FormNew.vue
@@ -1,3 +1,10 @@
 <template>
-    <h1>New Form</h1>
+    <div class="container">
+        <h1>Cadastrar Endereco</h1>
+        <endereco-form></endereco-form>
+    </div>
 </template>
+
+<script setup>
+import EnderecoForm from "../components/EnderecoForm.vue";
+</script>
