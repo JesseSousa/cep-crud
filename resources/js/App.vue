55e4bbb62e9ca6848b042a9b33f80cf9170c3544
JesseSousa <rhadamesjesse14rj@gmail.com>
2022-04-17 18:32:30 -0300

    add navbar

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,4 +1,11 @@
 <template>
+    <nav class="navbar navbar-exand-lg navbar-dark bg-secondary mb-3">
+        <div class="container">
+            <router-link to="/" class="navbar-brand fw-bold fs-3">
+                CRUD CEP
+            </router-link>
+        </div>
+    </nav>
     <router-view></router-view>
 </template>
 
